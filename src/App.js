d2c383bc18c7d47256fd8c26a22b131dfd0ffd66
hossam <frontend@flows.sa>
2020-09-20 21:19:48 +0200

    updated Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Fragment } from 'react';
 
-function App() {
+import './App.css';
+const Todo = ({ text, completed, index, onCompelte }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <tr>
+        <td style={completed ? { textDecoration: "line-through" } : null}>
+          {text}
+        </td>
+        <td>
+          <input type="checkbox" disabled={completed} onClick={() => onCompelte(index)} />
+        </td>
+      </tr>
+    </Fragment>
+  )
+};
+
+const App = () => {
+  const [todos, setTodo] = useState([
+    { text: "go to walk", completed: false },
+    { text: "go to walk", completed: false },
+
+  ]);
+
+  const [formData, setFormData] = useState({
+    text: '',
+  })
+  const onCompelte = (e) => {
+    const newTodos = [...todos];
+    newTodos[e].completed = true;
+    setTodo(newTodos);
+  }
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setTodo([...todos, { text: formData.text, completed: false }])
+  };
+  return (
+    <Fragment>
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>
+                Todo
+              </th>
+              <th>
+                Completed
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo, index) => <Todo text={todo.text} completed={todo.completed} index={index} onCompelte={onCompelte} />)}
+          </tbody>
+        </table>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="text" >Todo</label>
+            <input type="text" name="text" value={formData.text} onChange={e => onChange(e)} className="form-control" placeholder="Enter Your Todo" />
+            <button type="submit" className="btn btn-success" >Add Todo </button>
+          </div>
+        </form>
+      </div>
+    </Fragment>
   );
 }
 
